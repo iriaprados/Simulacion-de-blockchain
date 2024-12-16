6280aa2e0a9d56adc690ba6dee1f76def9d14710
IRIAP <iria.prados@gmail.com>
2024-12-16 21:11:20 +0100

    Programa con todas las funciones falta estilizarlo y mejorar algunas Prados_Cepero_Iria_ej_3_PEC2_SSDD

--- Prados_Cepero_Iria_ej_3_PEC2_SSDD.py
+++ Prados_Cepero_Iria_ej_3_PEC2_SSDD.py
@@ -10,7 +10,7 @@ mutex = threading.Lock()  # Crear mutex
 blockchain = []  # Lista que simula el blockchain, para el almacenamiento de los bloques
 probabilidades = {"Nodo 1": 50, "Nodo 2": 30, "Nodo 3": 20}  # Probabilidad de cada nodo
 
-# Creación de la clase block
+# Creación de la clase block, crea un bloque
 class block:
     def __init__(self, index, timestamp, block_hash, previous_hash, validator):
         self.index = index  # Posición del bloque en la cadena
@@ -23,7 +23,7 @@ class block:
         return (f'Bloque (Indice: {self.index}, Marca de tiempo: {self.timestamp}, Hash: {self.block_hash}, Hash previo: {self.previous_hash}, Nodo creador: {self.validator})\n')
 
 
-# Función proof_of_stake()
+# Función proof_of_stake(), selecciona el nodo que puede participar 
 def proof_of_stake():
     # Convertir los valores del diccionario probabilidades en listas
     nodos = list(probabilidades.keys())  # Nombre de los nodos
@@ -37,7 +37,7 @@ def proof_of_stake():
     return seleccion
 
 
-# Función para la sección crítica
+# Función para la sección crítica, simula creación del bloque 
 def critical_section(productor, particiones, mutex):
     print(f'\nEl nodo {productor} intenta acceder a la sección crítica del bloque')
 
@@ -50,47 +50,68 @@ def critical_section(productor, particiones, mutex):
     else:
         bloque_anterior = None  # En la blockchain no hay un bloque anterior
 
-    timestamp = int(time.time())  # Tiempo actual (segundos)
+    timestamp = int(time.time())  # Generar el tiempo actual (segundos)
 
     if bloque_anterior:
         hash_anterior = bloque_anterior.block_hash  # Si hay un bloque previo en el blockchain, se obtiene su hash
     else:
         hash_anterior = '0'  # Si no hay bloque anterior, se indica con cero
 
-   
-    datos = str(len(blockchain)) + str(particiones) + str(timestamp) + hash_anterior  # Datos para generar el hash
+   # Generación del hash 
+   # Datos: ID del nodo, particiones del nodo, timestamp en segundos, hash del bloque anterior
+    datos = str(len(blockchain)) + str(particiones) + str(timestamp) + hash_anterior 
 
-    block_hash = sha256(datos.encode()).hexdigest() # Hash para los datos 
+    hash = sha256(datos.encode()).hexdigest() # Hash generado apartir de los datos  
 
-    nuevo_bloque = block(len(blockchain), timestamp, block_hash, hash_anterior, productor) # Creación del bloque 
+    nuevo_bloque = block(len(blockchain), timestamp, hash, hash_anterior, productor) # Creación del bloque. 
 
     blockchain.append(nuevo_bloque)
 
-    time.sleep(2)
+    time.sleep(2) # Simulación del trabajo 
 
     mutex.release() # Liberar el mutex
 
     print(f'Bloque {nuevo_bloque.index} producido, el nodo {productor} liberado el mutex\n')
 
+# Función para definir el compartamiento de lso nodos 
+def run_node(productor, mutex): 
+    print(f'El nodo {productor} ha sido iniciado')
+
+    while len(blockchain) < blocks: # Mientras la blockchain contenga menos bloques que el objetivo (12) 
+        print(f'El nodo {productor}, esta esperando para producir un bloque')
+
+        nodo_productor = proof_of_stake()
+
+        if nodo_productor == productor: 
+            print(f'El nodo {productor} ha sido seleccionado para producir un bloque') 
+            critical_section(productor, len(blockchain), mutex)
+        time.sleep(1)
 
 # Función principal que simula el proceso de producción de bloques
 def main():
-    for i in range(5):
-        print(f'Selección {i + 1}:')
-        productor = proof_of_stake()
-
-        # Generar hilos 
-        thread = threading.Thread(target=critical_section, args=(productor, i, mutex)) # Hilos con target la sección crítica 
-        thread.start()
-        thread.join() 
+    hilos= [] # Inciar todos los hilos 
+    for i in range(1, num+1): # Para cada uno de los nodos genrados, se crea un hilo
+        hilo= threading.Thread(target=run_node, args=(f'Nodo {i}', mutex)) # Los hilos con target la función run_node
+        hilos.append(hilo)
+        hilo.start()
+    
+    for hilo in hilos: # Esperar que todos los hilos terminen 
+        hilo.join()
+    
+     # Imprimir la cadena de bloques una vez todos los bloques hayan sido producidos
+    print("\nBlockchain generada:")
+    for block in blockchain:
+        print(f"Índice: {block.index}, Timestamp: {block.timestamp}, Hash: {block.block_hash}, Productor: {block.validator}, Hash previo: {block.previous_hash}")
+    
+  
 
 
 if __name__ == "__main__":
-    # Crear el bloque génesis
-    genesis_block = block(0, time.time(), "hash_inicial", None, "Node-1")
-    blockchain.append(genesis_block)  # Añadir el bloque génesis
+    # # Crear el bloque génesis
+    # genesis_block = block(0, time.time(), "hash_inicial", None, "Node-1")
+    # blockchain.append(genesis_block)  # Añadir el bloque génesis
 
-    print(genesis_block)  # Muestra los datos del bloque génesis
+    # print(genesis_block)  # Muestra los datos del bloque génesis
 
     # Llamar a la función principal
     main()
